e6334212581ddd5dea629a5e5e83cada2d341609
nicholas brickey <tmrogers1403@yahoo.com>
2022-12-06 22:59:35 -0500

    Deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+mechacar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+?lm()
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg)
+?summary()
+summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg))
