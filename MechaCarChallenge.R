3565aaf2a725107f0070de2da16379e1eb91ebbd
nicholas brickey <tmrogers1403@yahoo.com>
2022-12-07 12:02:43 -0500

    Part 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,8 @@ mechacar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg)
 ?summary()
 summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg))
+Suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+?summarise()
+total_summary <- Suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+?group_by()
+lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
